65c95088ae7f6cf8a6199324555479035f951451
Mariam <codegirlbxl@gmail.com>
2026-06-11 15:02:55 +0200

    feat: add pages and configure routing

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,27 @@
 function Contact() {
-  return <div>Contact</div>;
+  return (
+    <>
+      <div>Contact</div>;
+      <lu>
+        <li>
+          <a href="https://github.com/vitejs/vite" target="_blank">
+            <svg className="button-icon" role="presentation" aria-hidden="true">
+              <use href="/icons.svg#github-icon"></use>
+            </svg>
+            GitHub
+          </a>
+        </li>
+        <li>
+          <a href="https://chat.vite.dev/" target="_blank">
+            <svg className="button-icon" role="presentation" aria-hidden="true">
+              <use href="/icons.svg#discord-icon"></use>
+            </svg>
+            Discord
+          </a>
+        </li>
+      </lu>
+    </>
+  );
 }
 
 export default Contact;
