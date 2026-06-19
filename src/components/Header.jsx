16068bebcbef8c2cae23aa4eb874710b51b2c7fa
Mariam <codegirlbxl@gmail.com>
2026-06-19 11:35:50 +0200

    add link fontawesome into index.html

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,54 +5,58 @@ function Header() {
   return (
     <>
       <header className="header">
-        <NavLink
-          to="/"
-          className={({ isActive }) =>
-            isActive ? "nav-link active" : "nav-link"
-          }
-        >
-          Home
-        </NavLink>
-        <NavLink
-          to="/projects"
-          className={({ isActive }) =>
-            isActive ? "nav-link active" : "nav-link"
-          }
-        >
-          Projects
-        </NavLink>
-        <NavLink
-          to="/skills"
-          className={({ isActive }) =>
-            isActive ? "nav-link active" : "nav-link"
-          }
-        >
-          Skills
-        </NavLink>
-        <NavLink
-          to="/experience"
-          className={({ isActive }) =>
-            isActive ? "nav-link active" : "nav-link"
-          }
-        >
-          Experience
-        </NavLink>
-        <NavLink
-          to="/languages"
-          className={({ isActive }) =>
-            isActive ? "nav-link active" : "nav-link"
-          }
-        >
-          Languages
-        </NavLink>
-        <NavLink
-          to="/contact"
-          className={({ isActive }) =>
-            isActive ? "nav-link active" : "nav-link"
-          }
-        >
-          Contact
-        </NavLink>
+        <button className="logo">Mariam Omri</button>
+
+        <nav>
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              isActive ? "nav-link active" : "nav-link"
+            }
+          >
+            Home
+          </NavLink>
+          <NavLink
+            to="/projects"
+            className={({ isActive }) =>
+              isActive ? "nav-link active" : "nav-link"
+            }
+          >
+            Projects
+          </NavLink>
+          <NavLink
+            to="/skills"
+            className={({ isActive }) =>
+              isActive ? "nav-link active" : "nav-link"
+            }
+          >
+            Skills
+          </NavLink>
+          <NavLink
+            to="/experience"
+            className={({ isActive }) =>
+              isActive ? "nav-link active" : "nav-link"
+            }
+          >
+            Experience
+          </NavLink>
+          <NavLink
+            to="/languages"
+            className={({ isActive }) =>
+              isActive ? "nav-link active" : "nav-link"
+            }
+          >
+            Languages
+          </NavLink>
+          <NavLink
+            to="/contact"
+            className={({ isActive }) =>
+              isActive ? "nav-link active" : "nav-link"
+            }
+          >
+            Contact
+          </NavLink>
+        </nav>
       </header>
     </>
   );
